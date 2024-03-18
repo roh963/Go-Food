8920152d9ac362a1bffa7943ae78f1470178fc52
rohan <khem7869218659@gmail.com>
2024-03-18 14:15:52 +0530

    creating schema and fetch a data from database

--- server/db.js
+++ server/db.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
- const mongdb=  async ()=> {
- try {
-    await mongoose.connect(process.env.Db);
-    console.log('Connected to MongoDB');
-} catch (error) {
-    console.error('Error connecting to MongoDB:');
-}
- }
- 
- module.exports= mongdb;
+
+const mongdb = async () => {
+    try {
+        await mongoose.connect(process.env.Db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        const collection = await mongoose.connection.db.collection("food_items");
+        const data = await collection.find({}).toArray();
+        console.log("Connected to MongoDB Atlas database",data);
+    } catch (error) {
+        console.error("Error connecting to MongoDB Atlas database:", error);
+    }
+};
+
+module.exports = mongdb;

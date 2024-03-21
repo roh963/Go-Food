4eaaa7ab4ed48ec8948c26873e319cf1e1b5e289
rohan <khem7869218659@gmail.com>
2024-03-22 00:36:17 +0530

    signup and login code write in bakend side

--- server/index.js
+++ server/index.js
@@ -2,16 +2,26 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = process.env.Port;
+const fport = process.env.fPort;
 const mongodb = require("./db");
 const createuser = require("./routes/createuser")
 mongodb();
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin",fport),
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next()
+
+})
+app.use(express.json());
+app.use("/api",createuser)
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.use(express.json());
-app.use("/api",createuser)
 
 app.listen(port,()=>{
     console.log(`listen at port ${port}`);
--- server/routes/createuser.js
+++ server/routes/createuser.js
@@ -25,5 +25,26 @@ router.post("/createuser", [EmailVal, NameVal, PassVal], async (req, res) => {
         res.json({ success: false });
     }
 });
+router.post("/loginuser", [EmailVal, PassVal], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const email = req.body.email;
+    try {
+      const userdata =  await User.findOne({email})
+        if (!userdata) {
+            return res.status(400).json({ errors: "Try logging with correct credential" });
+        }
+        if (req.body.password !== userdata.password) {
+            return res.status(400).json({ errors: "Try logging with correct credential" });
+        }
+        res.json({ success: true });
+
+    } catch (e) {
+        console.error(e);
+        res.json({ success: false });
+    }
+});
 
 module.exports = router;
